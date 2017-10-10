21c08ee967f3dee402b21530598ee5260e93e685
AndrewB82 <32488111+AndrewB82@users.noreply.github.com>
2017-10-10 13:00:55 +0200

    Update Cam_v2.php

--- PHP/Cam_v2.php
+++ PHP/Cam_v2.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<!-- Cameras' streaming site. It works with Foscam FI916P v. 2 camera.
+<!-- Cameras' streaming site. It works with Foscam FI9816P v.2 camera.
 WARNING: remember to provide actual camera IP with port and camera login credentials, 
 Foscam "GUEST" privilleges are sufficient in this case.  -->
 
@@ -26,7 +26,7 @@ Foscam "GUEST" privilleges are sufficient in this case.  -->
     <li class="col-6"><a id="Sceny" class="myButtonYellow" href="/BrowarRemote_v2.php">Sceny</a></li>
 </ul>
 <div class="section" id="KameraDziewczynek">
-    <div class="col-12 myCaption">Pok�j Dziewczynek</div>
+    <div class="col-12 myCaption">Pokój Dziewczynek</div>
 <!-- //Below actual camera IP with port, camera login and password must be provided, user with Foscam "GUEST" privilleges is sufficient in this case. -->
     <div class="col-12"><img width="100%" src="http://<camera IP>:<camera port>/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=<login>&pwd=<password>&t=" name="refresh" id="refresh" onload='reload(this)' onerror='reload(this)'></div>
     <div class="col-20percent"><a id="ptzMoveLeft" class="myButtonBlue">&#x02c2</a></div>
